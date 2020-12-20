d0ea090eb99eb96ca8a69e7dd3c10aea80b9d054
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-20 17:52:26 +0330

    added: setup.py.
    details:
    now users can easily install the program by running this setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='youtube2dideo',
+    version='1.0.0',
+    description='This program converts youtube video links to dideo.ir  ',
+    scripts= ["convert-dideo.py"]
+)
