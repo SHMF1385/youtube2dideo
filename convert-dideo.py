ead0028d12588d24ed48ca6c96e02cb2d77805e6
A.Ahani <alirezaahani@protonmail.com>
2021-02-05 15:55:12 +0330

    Update convert-dideo.py

--- convert-dideo.py
+++ convert-dideo.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import subprocess
 from webbrowser import open as open_browser
 
 def CheckUrl(url):
@@ -23,11 +24,22 @@ def CheckUrl(url):
         print("Link is broken.")
         return False
 
+def DownloadVideo(url):
+    print("Ruuning youtube-dl with given url ...")
+    command = ['youtube-dl',url]
+    try:
+        print("Ruuning youtube-dl with given url ...")
+        subprocess.run(command)
+    except FileNotFoundError:
+        print("Youtube-dl not found!")
+
+
 
 parser = argparse.ArgumentParser(description="converts youtube links into dideo links")
 parser.add_argument("url", help="youtube video url")
-parser.add_argument("--check", dest="check", action="store_const", const=True, default=False)
-parser.add_argument("--open-in-browser", dest="open", action="store_const", const=True, default=False)
+parser.add_argument("--check", dest="check", action="store_const", const=True, default=False,help='Checks if the link is broken')
+parser.add_argument("--open-in-browser", dest="open", action="store_const", const=True, default=False,help='Opens the converted link in browser')
+parser.add_argument("--download", dest="download", action="store_const", const=True, default=False,help='Downloads the video with youtube-dl')
 args = parser.parse_args()
 
 
@@ -38,7 +50,10 @@ def main():
     video_code = args.url.split('?v=')[1]
     video_url = "https://www.dideo.ir/v/yt/{}/".format(video_code)
     print(video_url)
-    if args.check and CheckUrl(video_url) and args.open: 
+    if CheckUrl(video_url) and args.open: 
         open_browser(video_url)
+    if args.download and CheckUrl(video_url):
+        DownloadVideo(video_url)
+
 
 if __name__ == "__main__": main()
